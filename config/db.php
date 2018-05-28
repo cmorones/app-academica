a154e7c5b29e8c9d2ef80622c6823947913d2feb
Cesar Morones <cesar.morones1@gmail.com>
2018-05-28 18:28:21 -0500

    Conexion db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=gestion_docente',
+    'dsn' => 'pgsql:host=10.12.0.171;dbname=gestion_docente',
     'username' => 'kylix',
     'password' => '2020kylix',
     'charset' => 'utf8',
